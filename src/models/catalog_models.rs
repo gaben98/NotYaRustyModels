67b64c3d10ca04d76a3b4e2dcfcfe3ec66f5ccec
Gabriel <Gabriel@DESKTOP-DNH4IOT>
2026-06-23 18:30:57 -0400

    added root category to item model

--- src/models/catalog_models.rs
+++ src/models/catalog_models.rs
@@ -32,6 +32,7 @@ impl Default for Catalog {
 pub struct CatalogItem {
     pub item_name: String,
     pub category_ids: Vec<String>,
+    pub root_category_id: String,
     pub price: i32,
     pub image_ids: Vec<String>,
     pub html_description: String,
